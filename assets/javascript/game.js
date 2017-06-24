d8c0af273da1e9eb652eb088e46f4d72308f1d41
Terri Pannell <Terri Pannell>
2017-06-24 18:30:35 -0400

    final code update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	//Create and initialize global variables
 	var targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
 	var	numberGems = 4;
 	var imgStones = ["assets/images/diamond_PNG6680.png",
@@ -6,10 +7,10 @@ $(document).ready(function(){
 		"assets/images/ruby_PNG26.png",
 		"assets/images/sapphire_PNG22.png"];
 	var counter = 0;
-	var crystalValue = 0;
 	var wins = 0;
 	var losses = 0;
 
+	// Dynamically add gem images and current values
 	for(i=0;i<numberGems;i++)
 	{
 		var imgStoneImage = $("<img>");
@@ -21,9 +22,11 @@ $(document).ready(function(){
 	}
 
 	$("#target").text(targetNumber);
+	$("#message").text(" ");
 
 	function reset()
 	{
+		//Reset target, gems, and current score between each game
 		targetNumber = Math.floor(Math.random() * (120 - 19)) + 19;
 
 		$(".gems").empty();
@@ -39,27 +42,36 @@ $(document).ready(function(){
 		counter = 0;
 		$("#target").text(targetNumber);
 		$("#score").text(counter);
+
 	}
 
 
 	//gem will be added dynamically, so have to call on click this way
 	$('body').on('click', '.gem', function () {
-     				var bReset = false;
+     	var bReset = false;
+		var gemValue = 0;
+		
+		gemValue = $(this).attr("value");
 
-		crystalValue = $(this).attr("value");
-		counter += parseInt(crystalValue);
-
+		//add current gem's value to the total score and display it
+		counter += parseInt(gemValue);
 		$("#score").text(counter);
+
+
 		if(counter === targetNumber){
-			alert("you won");
+			//alert("you won");
 			bReset = true;
 			wins++;
+			$("#message").text("You Won!");
 		}
 		else if(counter > targetNumber){
-			alert("you lost");
+			//alert("you lost");
 			bReset = true;
 			losses++;
+			$("#message").text("You Lost!");
 		}
+		else
+			$("#message").empty();
 
 		if(bReset)
 		{
